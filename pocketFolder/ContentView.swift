285ca749019cf8212b469c882130fdef94ce97c9
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-10-29 13:36:14 +0100

    Initial Commit

---
+++ pocketFolder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pocketFolder
+//
+//  Created by @AwMan3703 (GitHub) on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pocketFolder/pocketFolderApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocketFolderApp.swift
+//  pocketFolder
+//
+//  Created by @AwMan3703 (GitHub) on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct pocketFolderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
