0062cc07c0fe7ac5d7967d03c11dd50f9ea7d472
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-10-30 13:54:40 +0100

    Further setup

---
+++ pocketFolder/FileListView.swift
@@ -0,0 +1,18 @@
+//
+//  FileListView.swift
+//  pocketFolder
+//
+//  Created by Daniele Brambilla on 30/10/24.
+//
+
+import SwiftUI
+
+struct FileListView: View {
+    var body: some View {
+        Text("Files here!")
+    }
+}
+
+#Preview {
+    FileListView()
+}
