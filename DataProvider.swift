2b5b91b8b17c5f27e4f12a062f19684df4668787
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-11-04 17:55:56 +0100

    modified DataProvider

--- DataProvider.swift
+++ DataProvider.swift
@@ -10,19 +10,28 @@ import SwiftUI
 import Foundation
 
 
-struct DataProvider {
-    public static func savePaths(paths: [PocketFolder]) {
-        // Encode and save to UserDefaults
-        if let encoded = try? JSONEncoder().encode(paths) {
-            UserDefaults.standard.set(encoded, forKey: "pocketedPaths")
-        }
+class DataProvider: ObservableObject {
+    @Published var paths: [PocketFolder] = DataProvider.loadPaths() {
+        // Automatically save whenever paths is updated
+        didSet { savePaths() }
     }
     
-    public static func loadPaths() -> [PocketFolder] {
+    
+    public func savePaths() {
+        // Encode and save to UserDefaults
+        if let encoded = try? JSONEncoder().encode(self.paths) {
+            UserDefaults.standard.set(encoded, forKey: "pocketedPaths")
+        }
+        print("Saved folders")
+    }
+    
+    private static func loadPaths() -> [PocketFolder] {
         // Decode from UserDefaults if data exists
         if let savedPaths = UserDefaults.standard.data(forKey: "pocketedPaths"),
            let decoded = try? JSONDecoder().decode([PocketFolder].self, from: savedPaths) {
+            print("Loaded folders")
             return decoded
         } else { return [] }
     }
 }
+
