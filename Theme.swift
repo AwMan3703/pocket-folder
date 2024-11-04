ab250d91207414109f0b49c6d4d4ac051268e98c
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-11-04 17:55:00 +0100

    added theme for colors and other graphic variables

---
+++ Theme.swift
@@ -0,0 +1,16 @@
+//
+//  Theme.swift
+//  pocketFolder
+//
+//  Created on 04/11/24.
+//  
+//
+    
+
+import Foundation
+import SwiftUI
+
+
+struct Theme {
+    static let gradientColors = [Color.blue, Color.teal, Color.green]
+}
