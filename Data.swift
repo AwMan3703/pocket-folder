d048913d571a039a7e0b5672c3dbc5f634ee60dd
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-11-05 13:35:35 +0100

    rename DataProvider.swift to Data.swift

---
+++ Data.swift
@@ -0,0 +1,37 @@
+//
+//  Data.swift
+//  pocketFolder
+//
+//  Created on 05/11/24.
+//  
+//
+
+import SwiftUI
+import Foundation
+
+
+class PocketFoldersManager: ObservableObject {
+@Published var folders: [PocketFolder] = PocketFoldersManager.loadFolders() {
+    // Automatically save whenever paths is updated
+    didSet { saveFolders() }
+}
+
+
+public func saveFolders() {
+    // Encode and save to UserDefaults
+    if let encoded = try? JSONEncoder().encode(self.folders) {
+        UserDefaults.standard.set(encoded, forKey: "pocketedPaths")
+    }
+    print("Saved folders")
+}
+
+private static func loadFolders() -> [PocketFolder] {
+    // Decode from UserDefaults if data exists
+    if let savedPaths = UserDefaults.standard.data(forKey: "pocketedPaths"),
+       let decoded = try? JSONDecoder().decode([PocketFolder].self, from: savedPaths) {
+        print("Loaded folders")
+        return decoded
+    } else { return [] }
+}
+}
+
